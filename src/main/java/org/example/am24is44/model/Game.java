53f9a0d2cd67843c9e5933f10c96d785fe561f11
Riccardo Cadario <riccardocadario@gmail.com>
2024-03-18 18:06:43 +0100

    player e game(get e set)

--- src/main/java/org/example/am24is44/model/Game.java
+++ src/main/java/org/example/am24is44/model/Game.java
@@ -3,12 +3,22 @@ package org.example.am24is44.model;public class Game {
     private Player[] players;
     private Player startingPlayer,currentPlayer;
     private GameStatus status;
+    private int numberOfPlayer=0;
 
     public Game(Player[] players,Player startingPlayer,Player currentPlayer,GameStatus status){
         this.players=players;
         this.startingPlayer=startingPlayer;
         this.currentPlayer=currentPlayer;
         this.status=status;
+    }
+
+    public Player getStartingPlayer(){return startingPlayer;}
+    public GameStatus getStatus(){return status;}
+
+    public void createNewPlayer(String nickName,Pion pion, DrawableCard[] hand, DrawableCard[][] playArea,ObjectiveCard secretObjective,int score){
+        Player newPlayer = new Player(nickName, pion, hand, playArea, secretObjective, score);
+        players[numberOfPlayer] = newPlayer;
+        numberOfPlayer++;
     }
 
 
