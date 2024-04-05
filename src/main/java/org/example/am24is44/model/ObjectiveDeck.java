915d5b1865fa1c2a84e14058641387f75846b578
edoardofumagalli01 <edoardoalberto.fumagalli@mail.polimi.it>
2024-04-05 16:09:54 +0200

    ObjectiveDeck fix

--- src/main/java/org/example/am24is44/model/ObjectiveDeck.java
+++ src/main/java/org/example/am24is44/model/ObjectiveDeck.java
@@ -2,12 +2,14 @@ package org.example.am24is44.model;
 
 import static org.example.am24is44.model.CardPoints.*;
 import static org.example.am24is44.model.Resource.*;
+import java.util.Stack;
 
 public class ObjectiveDeck {
-    private ObjectiveCard[] objectiveCards;
+    private Stack<ObjectiveCard> objectiveCards;
 
     // Constructor
     public ObjectiveDeck() {
+        this.objectiveCards = new Stack<>();
     }
 
     // Method to set objective cards
@@ -18,43 +20,37 @@ public class ObjectiveDeck {
         objective87[1][1] = FUNGI_KINGDOM;
         objective87[2][0] = FUNGI_KINGDOM;
         PatternObjectiveCard c87 = new PatternObjectiveCard(TWO, objective87);
-        this.objectiveCards[0] = c87;
+        objectiveCards.push(c87);
 
         Resource[][] objective88 = new Resource[3][3];
         objective88[0][0] = PLANT_KINGDOM;
         objective88[1][1] = PLANT_KINGDOM;
         objective88[2][2] = PLANT_KINGDOM;
         PatternObjectiveCard c88 = new PatternObjectiveCard(TWO, objective88);
-        this.objectiveCards[1] = c88;
+        objectiveCards.push(c88);
 
         Resource[][] objective89 = new Resource[3][3];
         objective89[0][2] = ANIMAL_KINGDOM;
         objective89[1][1] = ANIMAL_KINGDOM;
         objective89[2][0] = ANIMAL_KINGDOM;
         PatternObjectiveCard c89 = new PatternObjectiveCard(TWO, objective89);
-        this.objectiveCards[2] = c89;
+        objectiveCards.push(c89);
     }
 
     // Method to get objective cards
-    public ObjectiveCard[] getCards() {
+    public Stack<ObjectiveCard> getCards() {
         return objectiveCards;
     }
 
     // Method to remove an objective card
     public void removeCard(ObjectiveCard card) {
-        if (objectiveCards.length == 0) {
+
+        if (objectiveCards.empty()) {
             // Manage the empty deck case
             return;
         }
 
-        // Creation of a new array with a smaller size
-        ObjectiveCard[] newObjectiveCards = new ObjectiveCard[objectiveCards.length - 1];
+        ObjectiveCard pop = objectiveCards.pop();
 
-        // Copy all the cards except from the last one
-        for (int i = 0; i < newObjectiveCards.length; i++) {
-            newObjectiveCards[i] = objectiveCards[i];
-        }
-
-        objectiveCards = newObjectiveCards;
     }
 }
