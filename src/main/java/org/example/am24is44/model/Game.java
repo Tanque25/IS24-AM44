cbe18d654e7cdaaf2ffce66ab90648f2b385afe1
Riccardo Cadario <riccardocadario@gmail.com>
2024-04-05 16:32:01 +0200

    player e game(get e set)

--- src/main/java/org/example/am24is44/model/Game.java
+++ src/main/java/org/example/am24is44/model/Game.java
@@ -10,30 +10,6 @@ package org.example.am24is44.model;public class Game {
     private Card[] visibleCards;
     private Card[] starterDeck;
 
-//    public Game(Player[] players,Player startingPlayer,Player currentPlayer,GameStatus status){
-//        this.players=players;
-//        this.startingPlayer=startingPlayer;
-//        this.currentPlayer=currentPlayer;
-//        this.status=status;
-//    }
-//
-//    public Player getStartingPlayer(){return startingPlayer;}
-//    public GameStatus getStatus(){return status;}
-//
-//    public void createNewPlayer(String nickName, Pion pion, Card[] hand, Card[][] playArea, ObjectiveCard secretObjective, int score, int position){
-//        Player newPlayer = new Player(nickName, pion, hand, playArea, secretObjective, score,position);
-//        players[numberOfPlayer] = newPlayer;
-//        numberOfPlayer++;
-//    }
-//
-//    public Player getCurrentPlayer(){return currentPlayer;}
-//
-//    //selezione next player incrementando la position di 1 e % (modulo) player lenght per
-//    //coprire anche il caso in cui sia arrivato all'ultimo elemento
-//    public Player nextPlayer(Player currentPlayer) {
-//        int nextPlayerIndex = (currentPlayer.getPosition() + 1) % players.length;
-//        return players[nextPlayerIndex];
-//    }
 
     // Constructor
     public Game() {
@@ -42,8 +18,7 @@ package org.example.am24is44.model;public class Game {
 
     // Method to initialize the game
     private void initializeGame() {
-        //bella
-        // Implement initialization logic for the game
+
     }
 
     // Method to add a player to the game
