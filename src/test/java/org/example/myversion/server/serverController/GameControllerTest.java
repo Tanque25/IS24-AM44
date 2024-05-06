700bf8dca23607e10dbf6399af93f8d242a77bf4
camibongio <camillaangela.bongiovanni@mail.polimi.it>
2024-05-06 16:01:28 +0200

    gameControllerTest update

--- src/test/java/org/example/myversion/server/serverController/GameControllerTest.java
+++ src/test/java/org/example/myversion/server/serverController/GameControllerTest.java
@@ -1,5 +1,6 @@
 package org.example.myversion.server.serverController;
 
+import org.example.myversion.server.model.exceptions.InvalidChoiceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +24,23 @@ class GameControllerTest {
     }
 
     @Test
-    void avaibleNickname() {
+    void availableNickname() {// Setup
+        String nickname = "Giulio";
+        int result = gameController.checkNickname(nickname);
+        assertEquals(1, result, "checkNickname should return 1 when the nickname is available");
+
+    }
+
+    @Test
+    void noAvailableNickname(){
+
+        String nickname = "Giulio";
+        gameController.addPlayer(nickname); // Aggiunge un giocatore con lo stesso nickname
+
+        int result = gameController.checkNickname(nickname);
+
+        assertEquals(0, result, "checkNickname should return 0 when the nickname is already used by a player");
+        assertNotEquals(1, result);
     }
 
     @Test
@@ -31,7 +48,23 @@ class GameControllerTest {
     }
 
     @Test
-    void chooseNumberPlayer() {
+    void chooseNumberPlayer_caseTrue() {
+        int numPlayers = 4; // Un numero valido di giocatori
+
+        boolean result = false;
+        try {
+            result = gameController.chooseNumberPlayer(numPlayers);
+        } catch (InvalidChoiceException e) {
+            fail("An unexpected InvalidChoiceException was thrown");
+        }
+        // Assert
+        assertTrue(result, "chooseNumberPlayer should return true for a valid number of players");
+
+    }
+
+    @Test
+    void chooseNumberPlayer_caseFalse() {
+        int numPlayers = 100;
     }
 
     @Test
